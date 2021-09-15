e7dfe692bbb5f2a71f65a63998f6420af5561df8
Kimmo Halonen <78320316+HalonenK@users.noreply.github.com>
2021-09-15 20:19:02 +0300

    Add files via upload

---
+++ dec_to_bin.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void dec_to_bin(int bits, int number) {
+    for (int i = 0; i < bits; i++) {
+        if (number & 1 << (bits - i - 1)) {
+            printf("1");
+        }
+        else {
+            printf("0");
+        }
+    }
+    printf("\n");
+}
+
+int main(void) {
+    int number = 0;
+    int bits = 0;
+
+    while (1) {
+        printf("Bitteja (negatiivinen arvo sulkee ohjelman): ");
+        scanf("%d", &number);
+
+        if (bits < 0) {
+            break;
+        }
+
+        printf("Anna luku: ");
+        scanf("%d", &number);
+        int_to_bin(bits, number);
+    }
+    return 0;
+}
